c5f4751e1ebd6f247d054e1a65e5bc6c2fe072fb
Denis Bochev <denisbo4ev@gmail.com>
2025-01-04 18:09:34 +0200

    add-movies

---
+++ src/components/MovieList/MovieList.jsx
@@ -0,0 +1,5 @@
+const MovieList = () => {
+  return <div>Movie List Component</div>;
+};
+
+export default MovieList;
--- src/service/api.js
+++ src/service/api.js
@@ -1,50 +1,39 @@
 import axios from "axios";
 
-const API_KEY = "your_api_key_here";
+const API_TOKEN =
+  "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmMjRmMDNjY2YwYTY1MGY1NmQyNWFiNzdlNzc2ZDQ5NSIsIm5iZiI6MTczNTg5MTA2OS4xMDA5OTk4LCJzdWIiOiI2Nzc3OTg3ZDMyMWEzYTE2NmE3NDk0ZTUiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.Au8o-qnGToeBo2SDCU9e84NCfmb37TJoIgPFn4kvC7A";
 const BASE_URL = "https://api.themoviedb.org/3";
 
+const axiosInstance = axios.create({
+  baseURL: BASE_URL,
+  headers: {
+    Authorization: `Bearer ${API_TOKEN}`,
+  },
+});
+
 export const fetchTrendingMovies = async () => {
-  const { data } = await axios.get(`${BASE_URL}/trending/movie/day`, {
-    headers: {
-      Authorization: `Bearer ${API_KEY}`,
-    },
-  });
+  const { data } = await axiosInstance.get("/trending/movie/day");
   return data.results;
 };
 
 export const fetchMoviesByQuery = async (query) => {
-  const { data } = await axios.get(`${BASE_URL}/search/movie`, {
-    headers: {
-      Authorization: `Bearer ${API_KEY}`,
-    },
+  const { data } = await axiosInstance.get("/search/movie", {
     params: { query },
   });
   return data.results;
 };
 
 export const fetchMovieDetails = async (movieId) => {
-  const { data } = await axios.get(`${BASE_URL}/movie/${movieId}`, {
-    headers: {
-      Authorization: `Bearer ${API_KEY}`,
-    },
-  });
+  const { data } = await axiosInstance.get(`/movie/${movieId}`);
   return data;
 };
 
 export const fetchMovieCredits = async (movieId) => {
-  const { data } = await axios.get(`${BASE_URL}/movie/${movieId}/credits`, {
-    headers: {
-      Authorization: `Bearer ${API_KEY}`,
-    },
-  });
+  const { data } = await axiosInstance.get(`/movie/${movieId}/credits`);
   return data;
 };
 
 export const fetchMovieReviews = async (movieId) => {
-  const { data } = await axios.get(`${BASE_URL}/movie/${movieId}/reviews`, {
-    headers: {
-      Authorization: `Bearer ${API_KEY}`,
-    },
-  });
+  const { data } = await axiosInstance.get(`/movie/${movieId}/reviews`);
   return data;
 };
